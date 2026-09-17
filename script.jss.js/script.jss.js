69c8c2e7a36946a07f34da17904f353f58b19b37
saiyadllapalli-dev <saiyadllapalli@gmail.com>
2026-09-17 12:39:42 +0530

    Add files via upload

---
+++ script.jss.js/script.jss.js
@@ -0,0 +1,21 @@
+// Smooth scrolling for navigation links
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function (event) {
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+
+            event.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+
+        }
+
+    });
+
+});
